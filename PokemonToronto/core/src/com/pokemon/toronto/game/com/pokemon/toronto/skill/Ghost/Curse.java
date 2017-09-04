551c10cdeb2ab80223754f75d21c7e4a8eea1849
gregory-kelsie <gregory.kelsie@gmail.com>
2017-09-04 15:26:21 -0400

    Forgot to add Curse
    - Added Curse

---
+++ PokemonToronto/core/src/com/pokemon/toronto/game/com/pokemon/toronto/skill/Ghost/Curse.java
@@ -0,0 +1,90 @@
+package com.pokemon.toronto.game.com.pokemon.toronto.skill.Ghost;
+
+import com.pokemon.toronto.game.com.pokemon.toronto.Field.Field;
+import com.pokemon.toronto.game.com.pokemon.toronto.Field.SubField;
+import com.pokemon.toronto.game.com.pokemon.toronto.Pokemon.Pokemon;
+import com.pokemon.toronto.game.com.pokemon.toronto.animation.SkillAnimation;
+import com.pokemon.toronto.game.com.pokemon.toronto.animation.skill.TackleAnimation;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.EffectSkill;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.FailResult;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.SecondaryEffect;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.skill_effects.AttackEffect;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.skill_effects.DefenseEffect;
+import com.pokemon.toronto.game.com.pokemon.toronto.skill.skill_effects.SpeedEffect;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Gregory on 9/4/2017.
+ */
+
+public class Curse extends EffectSkill {
+    /**
+     * - Name: Curse
+     * - Type: Ghost
+     * - PP: 10
+     * - Cat: Misc
+     * - Accuracy: 100
+     */
+    public Curse() {
+        super(37, "Curse", 10, Pokemon.Type.GHOST, 100);
+        effects.add(new SpeedEffect(1,
+                SecondaryEffect.Target.SELF, 1, SecondaryEffect.StatDirection.DECREASE));
+        effects.add(new AttackEffect(1,
+                SecondaryEffect.Target.SELF, 1, SecondaryEffect.StatDirection.INCREASE));
+        effects.add(new DefenseEffect(1,
+                SecondaryEffect.Target.SELF, 1, SecondaryEffect.StatDirection.INCREASE));
+    }
+
+    /**
+     * Use Curse and return the move results
+     * The user scatters a big cloud of sleep-inducing dust around the target.
+     * @param skillUser The Pokemon using the skill
+     * @param enemyPokemon The enemy receiving the skill
+     * @param field The field for the battle.
+     * @param userField The field for the battle.
+     * @param enemyField The field for the battle.
+     * @param skillUserParty
+     * @return The move results.
+     */
+    public List<String> use(Pokemon skillUser, Pokemon enemyPokemon, Field field,
+                            SubField userField, SubField enemyField, boolean isFirstAttack, List<Pokemon> skillUserParty) {
+
+        if (skillUser.getTypeOne() == Pokemon.Type.GHOST || skillUser.getTypeTwo() == Pokemon.Type.GHOST) {
+            List<String> results = new ArrayList<String>();
+            enemyPokemon.giveCurse();
+            int damage = (int)Math.floor(skillUser.getHealthStat() / 2.0);
+            skillUser.subtractHealth(damage);
+            results.add(skillUser.getName() + " cut its own HP and" +
+                    "\nlaid a curse on " + enemyPokemon.getName());
+            return results;
+        } else {
+            return super.use(skillUser, enemyPokemon, field, userField, enemyField, isFirstAttack, skillUserParty);
+        }
+
+    }
+
+    /**
+     * Return Curse's skill animation.
+     * @param userAnimation Whether or not the skill's animation is from the user
+     *                      using the skill or the enemy using the skill.
+     * @return Curse's skill animation.
+     */
+    @Override
+    public SkillAnimation getAnimation(boolean userAnimation) {
+        //TODO: Check the user type
+        return new TackleAnimation(userAnimation);
+    }
+
+    @Override
+    public FailResult willFail(Pokemon skillUser, Pokemon enemyPokemon, Field field,
+                               SubField userField, SubField enemyField, boolean isFirstAttacker) {
+        if ((skillUser.getTypeOne() == Pokemon.Type.GHOST ||
+                skillUser.getTypeTwo() == Pokemon.Type.GHOST) &&
+                enemyPokemon.isCursed()) {
+            return new FailResult("It failed...");
+        }
+        return new FailResult(false);
+    }
+}
